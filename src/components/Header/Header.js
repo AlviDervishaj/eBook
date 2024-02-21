9a2c42a04daf490a8963e92cb3560d2b080a56c6
Alvi Dervishaj <alvidervishaj9@gmail.com>
2024-02-21 01:02:11 +0100

    Updated Css and other files to make it more modern and a easy to use website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,9 @@ import './Header.css';
 export function Header() {
   return (
     <header>
-      <a href="/">E-Book Generator</a>
+      <div id="header" style={{backgroundImage: "url('/eBook-1.jpg')"}}>
+        <a href="/">E-Book Generator</a>
+      </div>
     </header>
   );
 }
